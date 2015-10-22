9e3a5a50b66803b015be124868d42c5e02b99601
ZacharyJia <jia199474@gmail.com>
2015-10-22 15:11:41 +0800

    创建项目

---
+++ src/main/cit/bjtu/pinger/Main.java
@@ -0,0 +1,10 @@
+package cit.bjtu.pinger;
+
+/**
+ * Created by Administrator on 2015/10/22.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
